8606d2bd389fe7dc440097c963ea829126f0d985
Ben Krikler <b.krikler@cern.ch>
2018-03-16 21:22:33 +0100

    Switch alphatwirl repo to my own

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PYTHONPATH=$PWD:$PYTHONPATH
 # get alphatwirl from master branch
-pip install -U git+https://github.com/alphatwirl/alphatwirl.git \
+pip install -U git+https://github.com/benkrikler/alphatwirl.git \
   flake8
 
 make install
